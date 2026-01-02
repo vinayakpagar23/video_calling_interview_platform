3dab9d452141e630f5e5a1447fb56f877d427424
vinayak.p2 <vinayak.p@padel-india.com>
2026-01-02 12:59:56 +0530

    insert userdata to db

---
+++ backend/src/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    clerkId: {
+      type: String,
+      reuired: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
