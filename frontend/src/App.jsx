1036338d67cc6da8c68c889a741d5fd499a2184b
vinayak.p2 <vinayak.p@padel-india.com>
2026-01-10 20:05:10 +0530

    feat:home page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,23 @@ import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProblemsPage from "./pages/ProblemsPage";
 import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
-  const { isSignedIn } = useUser();
-  console.log(isSignedIn);
+  const { isSignedIn, isLoaded } = useUser();
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
         <Route
           path="/problems"
           element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
